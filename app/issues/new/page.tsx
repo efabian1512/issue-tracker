0ba0e49ea12b1feea5bba84220531c8b91a60ca6
efabian1512 <efabian1989@hotmail.com>
2024-03-06 14:29:17 -0400

    Refactor: Move inline function

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -2,7 +2,7 @@
 import { Button, Callout, TextField, Text } from '@radix-ui/themes';
 import SimpleMDE from "react-simplemde-editor";
 import "easymde/dist/easymde.min.css";
-import { useForm, Controller } from 'react-hook-form';
+import { useForm, Controller, FieldValues } from 'react-hook-form';
 import axios from 'axios';
 import { useRouter } from 'next/navigation';
 import { useState } from 'react';
@@ -20,13 +20,7 @@ const NewIssuePage = () => {
     const [error, setError] = useState('');
     const [isSubmitting, setSubmitting] = useState(false);
 
-  return (
-    <div  className="max-w-xl">
-        {error && <Callout.Root color="red" className="mb-5">
-                <Callout.Text>{error}</Callout.Text>
-            </Callout.Root>}
-        <form 
-            className="space-y-3" onSubmit={handleSubmit(async (data)=> {
+    const onSubmit = async (data: FieldValues)=> {
                 try {
                     setSubmitting(true);
                     await axios.post('/api/issues', data);
@@ -35,7 +29,16 @@ const NewIssuePage = () => {
                     setSubmitting(false);
                     setError('An unexpected error occurred.');
                 }
-            })}>
+            }
+
+
+  return (
+    <div  className="max-w-xl">
+        {error && <Callout.Root color="red" className="mb-5">
+                <Callout.Text>{error}</Callout.Text>
+            </Callout.Root>}
+        <form 
+            className="space-y-3" onSubmit={handleSubmit(onSubmit)}>
           <TextField.Root>
               <TextField.Input placeholder="Title" {...register('title')}/>
           </TextField.Root>
