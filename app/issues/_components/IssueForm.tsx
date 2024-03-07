f3a5d15c30e6cd4ab3074d85077741111d4a3347
efabian1512 <efabian1989@hotmail.com>
2024-03-07 15:22:44 -0400

    Build an API for updating issues

--- app/issues/_components/IssueForm.tsx
+++ app/issues/_components/IssueForm.tsx
@@ -1,28 +1,28 @@
 'use client'
 import ErrorMessage from '@/app/components/ErrorMessage';
-import dynamic from 'next/dynamic';
 import Spinner from '@/app/components/Spinner';
-import { createIssueSchema } from '@/app/validationSchemas';
+import { issueSchema } from '@/app/validationSchemas';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { Button, Callout, TextField } from '@radix-ui/themes';
 import axios from 'axios';
 import "easymde/dist/easymde.min.css";
+import dynamic from 'next/dynamic';
 import { useRouter } from 'next/navigation';
 import { useState } from 'react';
 import { Controller, FieldValues, useForm } from 'react-hook-form';
 // import SimpleMDE from "react-simplemde-editor";
-import { z } from 'zod';
 import { Issue } from '@prisma/client';
+import { z } from 'zod';
 
 const SimpleMDE = dynamic(() => import('react-simplemde-editor'), {ssr: false});
 
-type IssueFormData = z.infer<typeof createIssueSchema>;
+type IssueFormData = z.infer<typeof issueSchema>;
 
 
 
 const NewIssueForm = ({ issue }: {issue?: Issue}) => {
     const router = useRouter();
-    const {register, control,  handleSubmit, formState: { errors } } = useForm<IssueFormData>({resolver: zodResolver(createIssueSchema)});
+    const {register, control,  handleSubmit, formState: { errors } } = useForm<IssueFormData>({resolver: zodResolver(issueSchema)});
     const [error, setError] = useState('');
     const [isSubmitting, setSubmitting] = useState(false);
 
