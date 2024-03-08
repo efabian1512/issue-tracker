26b432552a882907ca20e6d6054cf26b03af1fe8
efabian1512 <efabian1989@hotmail.com>
2024-03-08 12:05:13 -0400

    Add assigned issues to prisma schema

--- app/issues/[id]/AssigneeSelect.tsx
+++ app/issues/[id]/AssigneeSelect.tsx
@@ -1,33 +1,41 @@
-'use client';
-import { Skeleton } from '@/app/components';
-import { User } from '@prisma/client';
-import { Select } from '@radix-ui/themes';
-import { useQuery } from '@tanstack/react-query';
-import axios from 'axios';
+"use client";
+import { Skeleton } from "@/app/components";
+import { User } from "@prisma/client";
+import { Select } from "@radix-ui/themes";
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
 
 const AssigneeSelect = () => {
-  
-  const {data: users, error, isLoading} = useQuery<User[]>({queryKey: ['users'],
-        queryFn: () => axios.get('/api/users').then(resp => resp.data),
-        staleTime: 60 * 1000,
-        retry: 3
+  const {
+    data: users,
+    error,
+    isLoading,
+  } = useQuery<User[]>({
+    queryKey: ["users"],
+    queryFn: () => axios.get("/api/users").then((resp) => resp.data),
+    staleTime: 60 * 1000,
+    retry: 3,
   });
- 
-  if(isLoading) return <Skeleton/>
-  
+
+  if (isLoading) return <Skeleton />;
+
   if (error) return null;
 
   return (
     <Select.Root>
-        <Select.Trigger placeholder="Assign..."/>
-          <Select.Content>
-            <Select.Group>
-              <Select.Label>Suggestions</Select.Label>
-              {users?.map(user => (<Select.Item key={user.id} value={user.id}>{user.name}</Select.Item>))}
-            </Select.Group>
-          </Select.Content>
+      <Select.Trigger placeholder="Assign..." />
+      <Select.Content>
+        <Select.Group>
+          <Select.Label>Suggestions</Select.Label>
+          {users?.map((user) => (
+            <Select.Item key={user.id} value={user.id}>
+              {user.name}
+            </Select.Item>
+          ))}
+        </Select.Group>
+      </Select.Content>
     </Select.Root>
-  )
-}
+  );
+};
 
 export default AssigneeSelect;
