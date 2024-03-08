66dde8490793b528f284073f45995da3e8ebc0fc
efabian1512 <efabian1989@hotmail.com>
2024-03-08 18:26:43 -0400

    sortIssues

--- app/issues/list/page.tsx
+++ app/issues/list/page.tsx
@@ -24,11 +24,17 @@ const IssuesPage = async ({
   const status = statuses.includes(searchParams.status)
     ? searchParams.status
     : undefined;
-  console.log(statuses);
+
+  const orderBy = columns
+    .map((column) => column.value)
+    .includes(searchParams.orderBy)
+    ? { [searchParams.orderBy]: "asc" }
+    : undefined;
   const issues = await prisma.issue.findMany({
     where: {
       status,
     },
+    orderBy,
   });
   return (
     <div>
