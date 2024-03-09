105e906221a632573a720844e963cef0f03b9b84
efabian1512 <efabian1989@hotmail.com>
2024-03-09 00:54:06 -0400

    Add metadata

--- app/issues/[id]/page.tsx
+++ app/issues/[id]/page.tsx
@@ -39,5 +39,14 @@ const IssueDetaiPage = async ({ params }: Props) => {
     </Grid>
   );
 };
+export async function generateMetadata({ params }: Props) {
+  const issue = await prisma.issue.findUnique({
+    where: { id: parseInt(params.id) },
+  });
 
+  return {
+    title: issue?.title,
+    description: "Details of issue " + issue?.id,
+  };
+}
 export default IssueDetaiPage;
